e4218b178f2059b597d9b35b8dff43299091b1b4
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2026-06-24 21:22:59 +0200

    Correzione configurazione base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/la-preghiera/',
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
   ],
 })
